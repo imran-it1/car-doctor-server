2382f658c25244878b47373fa79b839a3bafbadf
Imran Hossain Sakil <112363382+imran-it1@users.noreply.github.com>
2023-10-27 22:22:53 +0600

    Connect mongDB

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,7 +11,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/*         ALL API      */
+/*         MongoDB + ALL API      */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7o1h45b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Client
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect
+		await client.connect();
+		// Send ping
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
 // Check API
 app.get("/", (req, res) => {
@@ -19,5 +46,5 @@ app.get("/", (req, res) => {
 
 // Listen port
 app.listen(port, () => {
-	console.log(`App is listening on port ${port}`);
+	console.log(`Car doctor server is runnig on port ${port}`);
 });
