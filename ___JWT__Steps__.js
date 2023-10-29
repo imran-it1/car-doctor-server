8ad15777b448939d89b26a557e6464ed6317d88d
Imran Hossain Sakil <112363382+imran-it1@users.noreply.github.com>
2023-10-29 17:09:32 +0600

    verify token

--- ___JWT__Steps__.js
+++ ___JWT__Steps__.js
@@ -43,6 +43,50 @@
 	})
 );
 
- * 2. Axios a optons er moddhe amra { withCredential: true }
+ * 2. Get token from client side ====  Axios a optons er moddhe amra { withCredential: true }
+   3. Sen token to the server side =====  Axios a optons er moddhe amra { withCredential: true }
+ *
+ */
+
+/**
+ *
+ * ||||||||||| Verify TOken Middlewarae |||||||||||||
+ *
+ * 
+ * 1. create a middleware with name and a async  function with three paramete called req,res,next
+ * 
+ * 
+ * |||| PHASE ONE |||| >>>>>> Check token ace na nai??????
+ * 
+ * 1. get the stored token from client side by req.cookies?.token
+ * *** use optional chaning that help you to run code without error. Use the givern name in the cookie in client side.
+ * 2. if there is no token return and send a message to the user with status code. if there is token then go to the PHASE TWO
+ * 
+ * |||| PHASE TWO |||| >>>>> token thakle seta varify korte hobe and user info ta pathate hobe
+ * 
+ * 1. jwt.verify( token, secret, ( err, decoded ) => {
+ * 
+ * 	2. if(err){
+ * 		return a message to the user with a status code
+ * }
+ * 
+ * 	3. if not error that mean it now in decoded phase with a valid token. send the user info from the decoded for further verification
+ * 
+ * 	req.user = decoded
+//  * 	Don't forget to use next()
+ * } )
+ * 
+ * 
+ *||||| PHASE THREE ||||| >>>>>>> decoded theke paoa user er info cross check kore dekhte hobe current data request kora user er info er sathe
+ *
+ *  jate rahim mia er token diye karim mia informaion na niye jete pare
+ * 
+ *  $$$$ Jokhon kono uesr data request kore tar email ta amra decoded theke paoa email er sathe cross check kore dekhbo
+ * 	 Cross check decoded user email and qurey email that request data
+	if (req.query.email !== req.user.email) {
+		return res.status(403).send({ message: "Forbiden" });
+	}
+ * 
+
  *
  */
--- index.js
+++ index.js
@@ -19,6 +19,32 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// Created MiddleWare
+const logger = async (req, res, next) => {
+	console.log("Called", req.protocol, req.host, req.originalUrl);
+	next();
+};
+
+// Vrify token middleware
+
+const verifyToken = async (req, res, next) => {
+	const token = req.cookies?.token;
+	// console.log("Token in middlewarw", token);
+	if (!token) {
+		return res.status(401).send({ message: "unAuthorized" });
+	}
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			console.log(err);
+			return res.status(401).send({ message: "unAuthorize" });
+		}
+		console.log("Info from decoded", decoded);
+		req.user = decoded;
+		next();
+	});
+};
+
 /*         MongoDB + ALL API      */
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7o1h45b.mongodb.net/?retryWrites=true&w=majority`;
@@ -35,25 +61,25 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		// Connect
-		await client.connect();
+		// await client.connect();
 
 		// Service Collection
 		const serviceCollection = client.db("carDoctor").collection("services");
 		const orderCollection = client.db("carDoctor").collection("orders");
 
 		// Auth related API
-		app.post("/jwt", async (req, res) => {
+		app.post("/jwt", logger, async (req, res) => {
 			try {
 				const user = req.body;
-				const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {});
+				const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+					expiresIn: "1h",
+				});
 				res
-					.cookie("accessToken", token, {
+					.cookie("token", token, {
 						httpOnly: true,
 						secure: false,
-						// sameSite: "none",
-						// eta dile google chrome a kaj kore na
 					})
-					.send({ success: true });
+					.send({ message: "success" });
 			} catch (error) {
 				console.error(error);
 			}
@@ -95,9 +121,15 @@ async function run() {
 		// Order related API
 
 		// Get specific order by query parameters
-		app.get("/orders", async (req, res) => {
+		app.get("/orders", verifyToken, async (req, res) => {
 			try {
-				// console.log(req.query);
+				// console.log("user from valid token", req.user);
+				// console.log("Request user", req.query.email);
+
+				// Cross check decoded user email and qurey email that request data
+				if (req.query.email !== req.user.email) {
+					return res.status(403).send({ message: "Forbiden access" });
+				}
 				let query = {};
 				if (req.query?.email) {
 					query = { customerEmail: req.query?.email };
